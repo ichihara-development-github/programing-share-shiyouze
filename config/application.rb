4f415cb05f82ac57845b204caedb28d57cfd8517
ichihara-development-github <haken.ichihara@icloud.com>
2020-05-03 22:59:05 +0900

    fdjajfa

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module MyBlog
 
     config.autoload_paths += %W(#{config.root}/lib/)
 
+    config.assets.compile = true
+
     config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
