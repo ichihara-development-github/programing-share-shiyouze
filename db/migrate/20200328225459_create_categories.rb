bc19c4653bef7af3e6d4711248cb3df0ce6cb678
ichihara-development-github <haken.ichihara@icloud.com>
2020-05-03 21:50:22 +0900

    dfma

--- db/migrate/20200328225459_create_categories.rb
+++ db/migrate/20200328225459_create_categories.rb
@@ -4,7 +4,6 @@ class CreateCategories < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :description
       t.string :image
-      t.references :article, foreign_key: true
       t.timestamps
     end
   end
