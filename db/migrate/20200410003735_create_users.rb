5d17c0f2ae8daea1717bb6c41440c22ae011ca34
ichihara-development-github <haken.ichihara@icloud.com>
2020-05-03 21:55:25 +0900

    dfma

--- db/migrate/20200410003735_create_users.rb
+++ db/migrate/20200410003735_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    drop_table :users
     create_table :users do |t|
       t.string :name
       t.string :password
-
+      t.string :password_digest
       t.timestamps
     end
   end
